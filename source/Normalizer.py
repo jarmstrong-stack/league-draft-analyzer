bec2874931fa0c810d51ec82396bb81f62ba362d
lipeeeee <filipe.pereirafga@gmail.com>
2024-09-18 21:00:42 +0100

    normalizer.py

---
+++ source/Normalizer.py
@@ -0,0 +1,11 @@
+"""
+    Data normalizer & pre processer for `league-draft-analyzer`
+
+    Will handle the data before being inputted into LDA neural net
+"""
+
+class Normalizer():
+    """Handles normalizing and preprocessing data dinamically"""
+
+    def __init__(self) -> None:
+        pass
